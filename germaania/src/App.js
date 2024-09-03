99ab6fcc6e83c988845d8332926e57a5de0245f4
pm <pm@digicart.hu>
2024-09-03 20:53:59 +0200

    tuning...

--- germaania/src/App.js
+++ germaania/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+const APP_NAME = "GerMánia";
+
+function Navbar(){
+  return (
+    <nav>
+      <a></a>
+    </nav>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{APP_NAME} | Kezdőlap</h1>
+        <Navbar />
       </header>
+
     </div>
   );
 }
